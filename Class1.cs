23d7952f325c10fd77b17ecdab526e55e472264e
rhsbalaram <rhsbalaram@gmail.com>
2017-01-24 19:13:30 +0530

    sh: q: command not found

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DeclaringConstants
+{
+    class Program2
+{
+    static void Main(string[] args)
+    {
+        const float pi = 3.14159f;
+
+            // constant declaration 
+            float r;
+        Console.WriteLine("Enter Radius: ");
+        r = Convert.ToSingle(Console.ReadLine());
+            float areaCircle = pi * r * r;
+        Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
+        Console.ReadLine();
+    }
+}
+}
