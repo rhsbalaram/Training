e57a16fd1be31054bf3d274bd6b6bf9a86854d82
rhsbalaram <rhsbalaram@gmail.com>
2017-01-24 16:02:20 +0530

    sh: q: command not found

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folder1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is out put");
+            int a = 10;
+            int b = 20;
+            b = a;
+            Console.WriteLine("");
+          long c=  sizeof(int);
+            object bd= 100;
+            
+            Console.WriteLine(b.ToString()+20);
+            Console.ReadKey();
+            b.ToString();
+           
+
+          
+
+        }
+    }
+}
